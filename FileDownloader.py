65e73df16a8388489089b0eb323b92425df354d4
Stef Belmans <belmansstef.codipack@gmail.com>
2020-05-04 13:38:29 +0200

    Browser en threading toegevoegd. Er zijn nog errors.

--- FileDownloader.py
+++ FileDownloader.py
@@ -41,8 +41,9 @@ class Downloader(QDialog):
         self.setWindowTitle("PyDownloader")
         self.setFocus()
 
-        download.clicked.connect(self.downloadthread.start)
+        download.clicked.connect(self.download)
         browse.clicked.connect(self.browse_file)
+        self.save_location.setText(r'C:\data\Leveranciers.C-schijf\Codipack\ArtikelLijsten\ImagesTest')
 
     def browse_file(self):
         ##save_file = QFileDialog.getSaveFileName(self,caption="Save File As", directory=("."),
@@ -54,37 +55,74 @@ class Downloader(QDialog):
         conn = pyodbc.connect('Driver={SQL Server};'
                               'Server=CODICPLPT039;'
                               'Database=TEST;'
-                              'Trusted_Connection=yes;')
-
+                              'Trusted_Connection=yes;'
+                              'MARS_Connection=Yes;')
+        conn2 = pyodbc.connect('Driver={SQL Server};'
+                              'Server=CODICPLPT039;'
+                              'Database=TEST;'
+                              'Trusted_Connection=yes;'
+                              'MARS_Connection=Yes;')
         cursor = conn.cursor()
-        cursor.execute("SELECT TOP (10) ManufacturerItemNumber, ItemImageURL FROM Tbl_scansource WHERE Manufacturer='Datalogic' ")
-        self.updater = conn.cursor()
-
+        cursor.execute("SELECT [dbo].[Tbl_Scansource].ManufacturerItemNumber, [dbo].[Tbl_Scansource].[ItemImageURL] FROM dbo.Tbl_Zebra LEFT OUTER JOIN dbo.Tbl_Scansource ON dbo.Tbl_Zebra.[product id] = [dbo].[Tbl_Scansource].[ManufacturerItemNumber] WHERE Tbl_Zebra.Picture = ''")
+        updater = conn2.cursor()
+        FailedImages = []
         for row in cursor:
+
             url = row[1]
             item = row[0]
-
+            print(item)
             #_thread.start_new_thread(self.download_thread, (row[1], row[0]))
             if url != "":
                 self.url.setText(url)
+                save_location_path = self.save_location.text() + "\\" + str(item) + ".jpg"
+                #print(save_location_path)
+                try:
+                    urllib.request.urlretrieve(url, save_location_path, self.report)
+                    self.progress.setValue(0)
+                    self.url.setText("")
+                    updater.execute("UPDATE [dbo].[Tbl_Zebra] SET [Picture] = ? WHERE [product id] = ?",
+                                    (item + ".jpg"), item)
+                    updater.commit()
 
+                except Exception:
+                    FailedImages.append(item)
+                    #print(item)
+                    #self.updater.execute(
+                    #    "UPDATE Tbl_scansource SET FileDownloaded=0, FileDownloadError=?  WHERE ManufacturerItemNumber = ?",
+                    #    "The download failed for item " + item, item)
+                    #QMessageBox.warning(self, "Warning", "The download failed")
 
-            #save_location = self.save_location.text() + "\\" + item + ".jpg"
-            #print(save_location)
-            #try:
-            #    urllib.request.urlretrieve(url, save_location, self.report)
-            #    #self.updater.execute("UPDATE Tbl_scansource SET FileDownloaded=1 WHERE ManufacturerItemNumber = ?",
-            #    #                     item)
-            #except Exception:
-            #    print("The download failed for item " + item)
-            #    #self.updater.execute(
-            #    #    "UPDATE Tbl_scansource SET FileDownloaded=0, FileDownloadError=?  WHERE ManufacturerItemNumber = ?",
-            #    #    "The download failed for item " + item, item)
-            #    # QMessageBox.warning(self, "Warning", "The download failed")
-        self.progress.setValue(0)
-        self.url.setText("")
-        self.save_location.setText("")
+        cursor.execute("SELECT [dbo].[Tbl_Jarltech].ORIGINAL_ART_NO, [dbo].[Tbl_Jarltech].[PICTURE] FROM dbo.Tbl_Zebra LEFT OUTER JOIN dbo.Tbl_Jarltech ON dbo.Tbl_Zebra.[product id] = [dbo].[Tbl_Jarltech].[ORIGINAL_ART_NO] WHERE Tbl_Zebra.Picture = ''")
+        for row in cursor:
+
+            url = row[1]
+            item = row[0]
+            print(item)
+            #_thread.start_new_thread(self.download_thread, (row[1], row[0]))
+            if url != "":
+                self.url.setText(url)
+                save_location_path = self.save_location.text() + "\\" + str(item) + ".jpg"
+                #print(save_location_path)
+                try:
+                    urllib.request.urlretrieve(url, save_location_path, self.report)
+                    self.progress.setValue(0)
+                    self.url.setText("")
+                    updater.execute("UPDATE [dbo].[Tbl_Zebra] SET [Picture] = ? WHERE [product id] = ?",
+                                    (item + ".jpg"), item)
+                    updater.commit()
+
+                except Exception:
+                    FailedImages.append(item)
+                    #print(item)
+                    #self.updater.execute(
+                    #    "UPDATE Tbl_scansource SET FileDownloaded=0, FileDownloadError=?  WHERE ManufacturerItemNumber = ?",
+                    #    "The download failed for item " + item, item)
+                    #QMessageBox.warning(self, "Warning", "The download failed")
+
+        ##self.save_location.setText("")
         QMessageBox.information(self, "Information", "All downloads are completed")
+        print("Failed downloads: " )
+        print(FailedImages)
 
     def download_thread(self, url, item):
         save_location = self.save_location.text() + "\\" + item + ".jpg"
